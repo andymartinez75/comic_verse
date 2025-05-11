461a8140e6654a3610dd2346eac1e17914afac56
Andrea Martinez <andyma31975@gmail.com>
2025-05-11 10:33:36 -0300

    agrego menu hamburguesa

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/nav.css';
+import { useState } from 'react';
+
 
 const Nav = () => {
+    const [menuAbierto, setMenuAbierto] = useState(false);
+
+    const toggleMenu = () => {
+    setMenuAbierto(!menuAbierto);
+    }
+
 return (
     <nav className="nav">
-    <ul>
-        <li><Link to="/">Inicio</Link></li>
-        <li><Link to="/about">Acerca de</Link></li>
-        <li><Link to="/contacto">Contacto</Link></li>
-        <li><Link to="/productos">Comics</Link></li>
-        <li><Link to="/carrito">Carrito</Link></li>
+        <button className="menu-toggle" onClick={toggleMenu}>
+        ☰
+        </button>
+    <ul className={`nav-links ${menuAbierto ? 'abierto' : ''}`} >
+        <li><Link to="/" onClick={() => setMenuAbierto(false)}>Inicio</Link></li>
+        <li><Link to="/about" onClick={() => setMenuAbierto(false)}>Acerca de</Link></li>
+        <li><Link to="/contacto" onClick={() => setMenuAbierto(false)}>Contacto</Link></li>
+        <li><Link to="/productos" onClick={() => setMenuAbierto(false)}>Comics</Link></li>
+        <li><Link to="/carrito" onClick={() => setMenuAbierto(false)}>Carrito</Link></li>
     </ul>
     </nav>
 );
